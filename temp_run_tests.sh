92e16602f0ecf69cdf8363d976e0380937bcafe6
Your Name <ezyang@mit.edu>
2025-03-09 21:21:02 -0400

    Update temporary script to run all tests

--- temp_run_tests.sh
+++ temp_run_tests.sh
@@ -3,8 +3,5 @@
 # Ensure we're in the project root directory
 cd "$(dirname "$0")"
 
-# Make our temporary script executable
-chmod +x run_trailing_whitespace_tests.sh
-
-# Run the whitespace handling tests
-python -m pytest test/test_edit_file.py::TestEditFile::test_edit_file_content_trailing_whitespace_match test/test_edit_file.py::TestEditFile::test_edit_file_content_mixed_whitespace_match -v
+# Run the tests
+python -m pytest
