8cb5988c06c9011a702f0967e039b7651ae935f6
Your Name <ezyang@mit.edu>
2025-03-09 21:20:00 -0400

    Create a script to run the new trailing whitespace tests

---
+++ run_trailing_whitespace_tests.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Ensure we're in the project root directory
+cd "$(dirname "$0")"
+
+# Run just the trailing whitespace tests
+python -m pytest test/test_edit_file.py::TestEditFile::test_edit_file_content_trailing_whitespace_match test/test_edit_file.py::TestEditFile::test_edit_file_content_mixed_whitespace_match -v
