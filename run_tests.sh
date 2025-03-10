c13976e3401dd07852b9f175cec0a057d494abab
Your Name <ezyang@mit.edu>
2025-03-09 21:22:06 -0400

    Update run_tests.sh with whitespace test options

--- run_tests.sh
+++ run_tests.sh
@@ -5,3 +5,6 @@ cd "$(dirname "$0")"
 
 # Run the tests
 python -m pytest test/
+
+# To run just the whitespace handling tests
+# python -m pytest test/test_edit_file.py::TestEditFile::test_edit_file_content_trailing_whitespace_match test/test_edit_file.py::TestEditFile::test_edit_file_content_mixed_whitespace_match -v
