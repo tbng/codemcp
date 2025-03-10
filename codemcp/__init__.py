45d04e43275aab187f0d477a4d055594294c666f
Edward Z. Yang <ezyang@mit.edu>
2025-03-10 16:14:47 -0400

    Add run_command to codemcp package exports

--- codemcp/__init__.py
+++ codemcp/__init__.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 
 from .main import codemcp, configure_logging, mcp, run
+from .shell import run_command
 
 __all__ = [
     "configure_logging",
     "run",
     "mcp",
     "codemcp",
+    "run_command",
 ]
