4bc9ddcb5596a9dd9f629a1b4b000f5eb1a298d2
Your Name <ezyang@mit.edu>
2025-03-09 21:20:43 -0400

    Simplify temporary test script

--- temp_run_tests.sh
+++ temp_run_tests.sh
@@ -3,6 +3,8 @@
 # Ensure we're in the project root directory
 cd "$(dirname "$0")"
 
-# Run the tests with verbose output
+# Make our temporary script executable
 chmod +x run_trailing_whitespace_tests.sh
-./run_trailing_whitespace_tests.sh
+
+# Run the whitespace handling tests
+python -m pytest test/test_edit_file.py::TestEditFile::test_edit_file_content_trailing_whitespace_match test/test_edit_file.py::TestEditFile::test_edit_file_content_mixed_whitespace_match -v
