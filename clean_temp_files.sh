ba0c7ac8de97393fcc91ebd79bf1fc08592cdfc9
Your Name <ezyang@mit.edu>
2025-03-09 21:22:16 -0400

    Create script to clean up temporary files

---
+++ clean_temp_files.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Remove temporary test scripts
+rm -f run_temp_tests.sh
+rm -f temp_run_tests.sh  
+rm -f run_trailing_whitespace_tests.sh
+rm -f $0  # Remove this script itself
+
+echo "Temporary files cleaned up"
