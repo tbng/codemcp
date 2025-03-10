8ab6a3aa9165d9df35b7b0129a72e0fcc6f395d1
Edward Z. Yang <ezyang@mit.edu>
2025-03-10 15:51:53 -0400

    Update test_format_code_with_git_changes to mock run_command instead of subprocess.run

--- test/test_format.py
+++ test/test_format.py
@@ -178,7 +178,7 @@ class TestFormatTool(unittest.TestCase):
     @patch("codemcp.tools.format.commit_changes")
     @patch("codemcp.tools.format._check_for_changes")
     @patch("codemcp.tools.format.is_git_repository")
-    @patch("subprocess.run")
+    @patch("codemcp.tools.format.run_command")
     def test_format_code_with_git_changes(
         self, mock_run, mock_is_git_repo, mock_check_changes, mock_commit
     ):
