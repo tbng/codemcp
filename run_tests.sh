2c26e2daf46770258fcbf8da96109ff5961a9a25
Edward Z. Yang <ezyang@mit.edu>
2025-03-10 22:30:39 -0400

    Get test config ready
    Signed-off-by: Edward Z. Yang <ezyang@mit.edu>

--- run_tests.sh
+++ run_tests.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
-
-# Ensure we're in the project root directory
-cd "$(dirname "$0")"
-
-# Run the tests
-python -m pytest test/
-
-# To run just the whitespace-only lines tests
-# python -m pytest test/test_edit_file.py::TestEditFile::test_edit_file_content_whitespace_only_lines test/test_edit_file.py::TestEditFile::test_edit_file_content_multiple_whitespace_only_lines -v
-
-# To run just the untracked files test
-# python -m pytest test/test_write_file.py::TestWriteFile::test_write_file_content_untracked_file -v
+set -e
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$SCRIPT_DIR"
+"${SCRIPT_DIR}/.venv/bin/python" -m pytest $@
