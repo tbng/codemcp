3237072520f8909e2af7039d98aeb65973195d62
Edward Z. Yang <ezyang@mit.edu>
2025-03-11 11:50:31 -0400

    Update run_command to accept a list of arguments instead of a single string

--- codemcp/tools/run_command.py
+++ codemcp/tools/run_command.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from typing import Optional
+from typing import List, Optional
 
 from .code_command import get_command_from_config, run_code_command
 
@@ -9,26 +9,27 @@ __all__ = [
 ]
 
 
-def run_command(
-    project_dir: str, command_type: str, arguments: Optional[str] = None
-) -> str:
+def run_command(project_dir: str, command_type: str, arguments: Optional[List[str]] = None) -> str:
     """Run a command that is configured in codemcp.toml.
 
     Args:
         project_dir: The directory path containing the codemcp.toml file
         command_type: The type of command to run (e.g., "format", "lint", "test")
-        arguments: Optional arguments to pass to the command
+        arguments: Optional list of arguments to pass to the command
 
     Returns:
         A string containing the result of the command operation
     """
     command_list = get_command_from_config(project_dir, command_type)
-
-    # If arguments are provided, append them to the command
+    
+    # If arguments are provided, extend the command with them
     if arguments and command_list:
         command_list = command_list.copy()
-        command_list.append(arguments)
-
+        command_list.extend(arguments)
+    
     return run_code_command(
-        project_dir, command_type, command_list, f"Auto-commit {command_type} changes"
+        project_dir, 
+        command_type, 
+        command_list, 
+        f"Auto-commit {command_type} changes"
     )
