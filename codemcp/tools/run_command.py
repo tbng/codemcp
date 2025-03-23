a424d390be68866318ebc53cc2d761bdec3f4062
Edward Z. Yang <ezyang@mit.edu>
2025-03-24 06:41:47 +0800

    refactor: update run_command.py to accept string instead of list
    In codemcp/tools/run_command.py instead of accepting List[str] only ever accept str and assume it is shlex.split'able. Update the prompt for the tool in init_project.py accordingly.
    
    ```git-revs
    9ac95b0  (Base revision)
    ec6dac7  Update run_command to only accept strings for arguments
    97a7ab8  Update RunCommand tool description in init_project.py
    e54bdf0  Update arguments description in init_project.py
    bf1ea0e  Remove string-to-list conversion of arguments in main.py
    8c92b9f  Update RunCommand call to handle both string and list arguments (for backward compatibility)
    HEAD     Auto-commit format changes
    ```
    
    codemcp-id: 187-refactor-update-run-command-py-to-accept-string-in
    ghstack-source-id: 8a5c5a323a2ba34c3c2fb20686ce437d0a5430cc
    Pull-Request-resolved: https://github.com/ezyang/codemcp/pull/168

--- codemcp/tools/run_command.py
+++ codemcp/tools/run_command.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import shlex
-from typing import List, Optional, Union
+from typing import Optional
 
 from .code_command import get_command_from_config, run_code_command
 
@@ -13,7 +13,7 @@ __all__ = [
 async def run_command(
     project_dir: str,
     command: str,
-    arguments: Optional[Union[List[str], str]] = None,
+    arguments: Optional[str] = None,
     chat_id: str = None,
 ) -> str:
     """Run a command that is configured in codemcp.toml.
@@ -21,9 +21,9 @@ async def run_command(
     Args:
         project_dir: The directory path containing the codemcp.toml file
         command: The type of command to run (e.g., "format", "lint", "test")
-        arguments: Optional arguments to pass to the command. If a single string is
-                  provided, it will be parsed into a list of arguments using shell-style
-                  tokenization (spaces separate arguments, quotes can be used for arguments
+        arguments: Optional arguments to pass to the command as a string. It will be
+                  parsed into a list of arguments using shell-style tokenization
+                  (spaces separate arguments, quotes can be used for arguments
                   containing spaces, etc.)
         chat_id: The unique ID of the current chat session
 
@@ -35,13 +35,8 @@ async def run_command(
     # If arguments are provided, extend the command with them
     if arguments and command_list:
         command_list = command_list.copy()
-
-        # If a single string is provided, parse it into a list of arguments
-        if isinstance(arguments, str):
-            parsed_args = shlex.split(arguments)
-            command_list.extend(parsed_args)
-        else:
-            command_list.extend(arguments)
+        parsed_args = shlex.split(arguments)
+        command_list.extend(parsed_args)
 
     return await run_code_command(
         project_dir, command, command_list, f"Auto-commit {command} changes", chat_id
