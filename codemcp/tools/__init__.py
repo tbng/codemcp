f034da358a28e9ac86f8835dfb34910583a6ea4d
Edward Z. Yang <ezyang@mit.edu>
2025-03-24 19:51:48 +0800

    Update
    [ghstack-poisoned]

--- codemcp/tools/__init__.py
+++ codemcp/tools/__init__.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python3
 # Implement code_command.py utilities here
 
+from .git_blame import git_blame
+from .git_diff import git_diff
+from .git_log import git_log
+from .git_show import git_show
 from .rm import rm_file
 
 __all__ = [
+    "git_blame",
+    "git_diff",
+    "git_log",
+    "git_show",
     "rm_file",
 ]
